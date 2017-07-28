74e2dd5c97bddf10f643ecafe8879b89c5bd20fa
tung_tung <tungnt@emotiv.vn>
2017-07-28 18:31:29 +0700

    update sdk 3.5 libraries for windows

--- examples_extra_prime/Java/src/com/emotiv/examples/ActivateLicense/ActivateLicense.java
+++ examples_extra_prime/Java/src/com/emotiv/examples/ActivateLicense/ActivateLicense.java
@@ -4,13 +4,13 @@ import java.text.SimpleDateFormat;
 import java.util.Scanner;
 
 import com.emotiv.Iedk.*;
-import com.emotiv.Iedk.EdkErrorCode;
 
 public class ActivateLicense {
 
     public static void main(String[] args) {
         
         System.out.println("Activate SDK License" );
+        int result = 0;
         
         Scanner input = new Scanner(System.in);
         
@@ -19,10 +19,6 @@ public class ActivateLicense {
         
         System.out.println("password: ");
         String password = input.nextLine();
-        
-        System.out.println("number of debit: ");
-        String debitNum = input.nextLine();
-        input.close();
         
         if (Edk.INSTANCE.IEE_EngineConnect("Emotiv Systems-5") != EdkErrorCode.EDK_OK
                 .ToInt()) {
@@ -42,40 +38,68 @@ public class ActivateLicense {
         }
         
         System.out.println("Logged in as " + userName);
-
         
+        //Please put license here
         String license_ID = "";
         
-        int result = Edk.INSTANCE.IEE_AuthorizeLicense(license_ID, Integer.parseInt(debitNum));
+        
+       //Debit Information
+        Edk.DebitInfos_t.ByReference debitInfos = new Edk.DebitInfos_t.ByReference();
+        
+        //Get Debit Information
+        result = Edk.INSTANCE.IEE_GetDebitInformation(license_ID, debitInfos);
+        if (result == EdkErrorCode.EDK_OK.ToInt())
+        {          
+            System.out.println("Remaining Sessions     : " + (debitInfos.remainingSessions & 0xffffffffL)); //convert from signed int to unsigned long
+            System.out.println("Daily debit limitation : " + debitInfos.daily_debit_limit);
+            System.out.println();
+
+            System.out.println("Total debits today     : " + debitInfos.total_debit_today);
+            System.out.println("Remaining time (seconds) before resetting daily debit limitation: " + debitInfos.time_reset );
+            System.out.println();
+        }
+        else
+        {
+        	System.out.println("Get Debit Information unsuccessfully");
+        }
+        
+        System.out.println("Authorize a license with number of debit");
+        
+        System.out.println("Number of debits: ");
+        String debitNum = input.nextLine();
+        input.close();
+        
+        //Authorize License with debit number
+        result = Edk.INSTANCE.IEE_AuthorizeLicense(license_ID, Integer.parseInt(debitNum));
         
         if(result == EdkErrorCode.EDK_LICENSE_NOT_FOUND.ToInt())
         {
-            System.out.println("EDK_LICENSE_NOT_FOUND" );
+            System.out.println("AuthorizeLicense: EDK_LICENSE_NOT_FOUND" );
             System.out.println();
         }
         else if (result == EdkErrorCode.EDK_LICENSE_ERROR.ToInt())
         {
-            System.out.println("EDK_LICENSE_ERROR" );
+            System.out.println("AuthorizeLicense: EDK_LICENSE_ERROR" );
             System.out.println();
         }
         else if (result == EdkErrorCode.EDK_LICENSE_EXPIRED.ToInt())
         {
-            System.out.println("EDK_LICENSE_EXPIRED" );
+            System.out.println("AuthorizeLicense: EDK_LICENSE_EXPIRED" );
             System.out.println();
         }
         else if (result == EdkErrorCode.EDK_LICENSE_REGISTERED.ToInt())
         {
-            System.out.println("EDK_LICENSE_REGISTERED" );
+            System.out.println("AuthorizeLicense: EDK_LICENSE_REGISTERED" );
             System.out.println();
         }
         else if (result == EdkErrorCode.EDK_LICENSE_DEVICE_LIMITED.ToInt())
         {
-            System.out.println("EDK_LICENSE_DEVICE_LIMITED" );
+            System.out.println("AuthorizeLicense: EDK_LICENSE_DEVICE_LIMITED" );
             System.out.println();
         }
         else if (result == EdkErrorCode.EDK_UNKNOWN_ERROR.ToInt())
         {
-            System.out.println("EDK_UNKNOWN_ERROR" );
+            System.out.println("AuthorizeLicense: EDK_UNKNOWN_ERROR" );
             System.out.println();
         }
         else
@@ -86,35 +110,36 @@ public class ActivateLicense {
         if (!(result == EdkErrorCode.EDK_OK.ToInt() || result == EdkErrorCode.EDK_LICENSE_REGISTERED.ToInt()))
             return ;
         
+        System.out.println("Get current license information");
+        
         Edk.LicenseInfos_t.ByReference licenseInfos = new Edk.LicenseInfos_t.ByReference();
         
         //Get License Information
-        result = Edk.INSTANCE.IEE_LicenseInformation(licenseInfos);
-        
+        result = Edk.INSTANCE.IEE_LicenseInformation(licenseInfos);       
 
         if (result == EdkErrorCode.EDK_LICENSE_EXPIRED.ToInt())
         {
-            System.out.println("EDK_LICENSE_EXPIRED" );
+            System.out.println("LicenseInformation: EDK_LICENSE_EXPIRED" );
             System.out.println();
         }
         else if (result == EdkErrorCode.EDK_OVER_QUOTA.ToInt())
         {
-            System.out.println("EDK_OVER_QUOTA" );
+            System.out.println("LicenseInformation: EDK_OVER_QUOTA" );
             System.out.println();
         }
         else if (result == EdkErrorCode.EDK_ACCESS_DENIED.ToInt())
         {
-            System.out.println("EDK_ACCESS_DENIED" );
+            System.out.println("LicenseInformation: EDK_ACCESS_DENIED" );
             System.out.println();
         }
         else if (result == EdkErrorCode.EDK_LICENSE_ERROR.ToInt())
         {
-            System.out.println("EDK_LICENSE_ERROR" );
+            System.out.println("LicenseInformation: EDK_LICENSE_ERROR" );
             System.out.println();
         }
         else if (result == EdkErrorCode.EDK_NO_ACTIVE_LICENSE.ToInt())
         {
-            System.out.println("EDK_NO_ACTIVE_LICENSE" );
+            System.out.println("LicenseInformation: EDK_NO_ACTIVE_LICENSE" );
             System.out.println();
         }
         else
@@ -127,58 +152,43 @@ public class ActivateLicense {
         String soft_limit_date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new java.util.Date(licenseInfos.soft_limit_date*1000L));
         String hard_limit_date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new java.util.Date(licenseInfos.hard_limit_date*1000L));
 
-        System.out.println("Date From               : " + date_from );
+        System.out.println("From date              : " + date_from );
         System.out.println();
-        System.out.println("Date To                 : " + date_to );
-        System.out.println();
-
-        System.out.println("Seat number             : " + licenseInfos.seat_count );
+        System.out.println("To   date              : " + date_to );
         System.out.println();
 
-        System.out.println("Total Quota             : " + licenseInfos.quota );
-        System.out.println("Total quota used        : " + licenseInfos.usedQuota );
+        System.out.println("Number of seats        : " + licenseInfos.seat_count );
         System.out.println();
 
-        System.out.println("Soft Limit Date         : " + soft_limit_date );
-        System.out.println("Hard Limit Date         : " + hard_limit_date );
+        System.out.println("Total quotas           : " + licenseInfos.quota );
+        System.out.println("Total used quotas      : " + licenseInfos.usedQuota );
+        System.out.println();
+
+        System.out.println("Soft Limit Date        : " + soft_limit_date );
+        System.out.println("Hard Limit Date        : " + hard_limit_date );
         System.out.println();
 
         if(licenseInfos.scopes == 1)
         {
-            System.out.println("License type        : " + "EEG" );
+            System.out.println("License type       : " + "EEG" );
             System.out.println();
         }
         else if (licenseInfos.scopes == 2)
         {
-             System.out.println("License type       : " + "PM" );
+             System.out.println("License type      : " + "PM" );
                 System.out.println();
         }
         else if (licenseInfos.scopes == 3)
         {
-            System.out.println("License type        : " + "EEG + PM" );
+            System.out.println("License type       : " + "EEG + PM" );
             System.out.println();
         }
         else
         {
-            System.out.println("License type        : " + "No type" );
+            System.out.println("License type       : " + "No type" );
             System.out.println();
         }
-        
-        //Debit Information
-        Edk.DebitInfos_t.ByReference debitInfos = new Edk.DebitInfos_t.ByReference();
-        
-        //Get Debit Information
-        result = Edk.INSTANCE.IEE_GetDebitInformation(license_ID, debitInfos);
-        
-
-        System.out.println("Remain Session          : " + debitInfos.remainingSessions);
-        System.out.println("Daily debit limit       : " + debitInfos.daily_debit_limit);
-        System.out.println();
-
-        System.out.println("Total debit a day       : " + debitInfos.total_debit_today);
-        System.out.println("Remained time (seconds) before resetting debit limitation a day: " + debitInfos.time_reset );
-        System.out.println();
-                
+               
     }
 
 }
